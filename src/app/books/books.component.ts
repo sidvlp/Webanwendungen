a3c9d66ecf4ad6d93beefaa3c1cea403705fed2e
Sid <sid.wannink@yahoo.de>
2023-11-21 13:31:09 +0100

    init

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-books',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent {
+
+}
